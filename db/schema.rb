4db2d18897feb612cb94e7b90b95d8e140b90f1c
Stephanie Grzona <stephanie@800ceoread.com>
2015-02-03 16:47:43 -0600

    Added User's First and Last Name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203222007) do
+ActiveRecord::Schema.define(version: 20150203222926) do
 
   create_table "auctions", force: true do |t|
     t.string   "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150203222007) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
