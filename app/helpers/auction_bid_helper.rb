2b0dac94bffb221bef1125d2d4e974c58b31192f
Stephanie Grzona <stephanie@800ceoread.com>
2015-02-11 12:22:41 -0600

    Added Auction Bid

---
+++ app/helpers/auction_bid_helper.rb
@@ -0,0 +1,2 @@
+module AuctionBidHelper
+end
---
+++ test/controllers/auction_bid_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AuctionBidControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
