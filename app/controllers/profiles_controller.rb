ed10ca733d1a7d09e38a89a5db91f53059f62870
Stephanie Grzona <stephanie@800ceoread.com>
2015-03-02 14:27:54 -0600

    Refactored profile controller so that they're being built off current_user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,23 @@
 
 class ProfilesController < ApplicationController
- 
+
 
   def my
-    @profile = current_user
+    @profile = nil
+    if current_user.profile
+      @profile = current_user.profile
+    else
+      redirect_to new_user_profile_path(current_user)
+    end
   end
 
   def new
-    @profile = Profile.new
+    @profile = current_user.build_profile
   end
 
   def create
-    @user = User.find(current_user)
-    @profile = Profile.new(profile_params)
+    @profile = current_user.build_profile(profile_params)
+
     if @profile.save
       redirect_to my_user_profile_path
     else
