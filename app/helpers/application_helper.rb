80f7b7d5ec576f7f8e0dca40a72ddd4796682ae5
Stephanie Grzona <stephanie@800ceoread.com>
2015-03-03 11:32:32 -0600

    New method for creating/editing profile link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
+  def profile_link_for_current_user
+    if current_user
+      if current_user.profile
+        link_to "Edit profile", edit_user_profile_path(current_user)
+      else
+        link_to "Create profile", new_user_profile_path(current_user)
+      end
+    end
+  end
 
 end
