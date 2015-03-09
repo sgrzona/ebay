144e1e1accc9cfd5c4d448e98053deaf6ddec2bf
Stephanie Grzona <stephanie@800ceoread.com>
2015-03-09 13:14:38 -0500

    Added Validates Image Presence

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,6 +11,7 @@ class Profile < ActiveRecord::Base
  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 
+   validates :image, presence: true
    validates :username, presence: true
    validates :first_name, presence: true
    validates :last_name, presence: true
