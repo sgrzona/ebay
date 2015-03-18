1e2d7c505d2f8a8dd93114b5a9222f0267146f92
Stephanie Grzona <stephanie@800ceoread.com>
2015-03-18 12:09:26 -0500

    Added Finished Auctions Method

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -3,13 +3,10 @@ class Auction < ActiveRecord::Base
   has_many :auction_bids
   before_create :set_expiration 
   before_save :set_expiration
-
   validates :title, :presence => true
-  
-  
-  
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
 
   def set_expiration
@@ -24,7 +21,18 @@ class Auction < ActiveRecord::Base
     where("expires_at > ?", Time.now)
   end
 
+  def finished_auctions
+    where("expires_at < ?", Time.now)
+  end
+
   def highest_auction_bid
     self.auction_bids.maximum("bid")
   end
+
+   def winner
+      if current_user.auction_bids.closed.where(winner: true)
+      else
+      current_user.auction_bids.open
+      end
+   end
 end
