68f6d2193f1ba6850b4b395a979afe306cb0f629
Stephanie Grzona <stephanie@800ceoread.com>
2015-03-23 12:37:31 -0500

    Removed Edit and Update

--- app/controllers/auction_bids_controller.rb
+++ app/controllers/auction_bids_controller.rb
@@ -1,22 +1,18 @@
 class AuctionBidsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
-  def my
-    @auction_bid = current_user.auction_bids.order("bid desc")
-  end
-
   def index
-  	@auction = Auction.find(params[:auction_id])
+    @auction = Auction.find(params[:auction_id])
     @auction_bids = @auction.auction_bids.order("bid desc")
   end
 
   def new
-  	@auction = Auction.find(params[:auction_id])
+    @auction = Auction.find(params[:auction_id])
     @auction_bid = AuctionBid.new
   end
 
   def show
-  	@auction_bid = AuctionBid.find(params[:id])
+    @auction_bid = AuctionBid.find(params[:id])
     @auction = @auction_bid.auction
   end
 
@@ -34,18 +30,18 @@ class AuctionBidsController < ApplicationController
         render :new
       end
     else
-      flash[:error] = "You cannot bid on your own auction."
+      flash[:error] = "You cannot bid on your won auction."
       redirect_to @auction
     end
   end
 
-  private
+   private
 
   def auction_bid_params
-  	params.require(:auction_bid).permit(:bid)
+    params.require(:auction_bid).permit(:bid)
   end
 
   def not_users_own_auction?
-  	@auction_bid.auction.user != current_user
+    @auction_bid.auction.user != current_user
   end
 end
